aa911032a9ec31eb26f79b443b8690881596ee68
Lucas Biagini <biaginisilva@gmail.com>
2018-06-20 10:59:32 -0300

    Initial Commit

---
+++ projeto-foundations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projeto-foundations
+//
+//  Created by Lucas Biagini on 20/06/18.
+//  Copyright © 2018 AcademyFoundations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
