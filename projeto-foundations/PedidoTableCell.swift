8d3937cab3c860ec06459b311e87e36dd0d5980c
Lucas Biagini <biaginisilva@gmail.com>
2018-06-20 11:58:39 -0300

    Updating .gitignore

---
+++ projeto-foundations/PedidoTableCell.swift
@@ -0,0 +1,9 @@
+//
+//  PedidoTableCell.swift
+//  projeto-foundations
+//
+//  Created by Lucas Biagini on 20/06/18.
+//  Copyright © 2018 AcademyFoundations. All rights reserved.
+//
+
+import Foundation
