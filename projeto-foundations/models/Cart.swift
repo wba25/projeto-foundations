40a3374cb689e99a2d52e867b411b351443b517a
Lucas Biagini <biaginisilva@gmail.com>
2018-06-27 11:38:07 -0300

    Added new getters and setters to Cart model

--- projeto-foundations/models/Cart.swift
+++ projeto-foundations/models/Cart.swift
@@ -13,6 +13,17 @@ class Cart {
     
     var name: String
     var url: String
+    var preenchimento: String
+    var espessura: String
+    var material: String
+    var quantidade: String
+    var rua: String
+    var numero: String
+    var complemento: String
+    var cep: String
+    var creditCard: String
+    var cvv: String
+    var price: String
     
     
     private static var sharedCart: Cart = {
@@ -24,6 +35,33 @@ class Cart {
     private init() {
         self.name = ""
         self.url = ""
+        self.preenchimento = ""
+        self.espessura = ""
+        self.material = ""
+        self.quantidade = ""
+        self.rua = ""
+        self.numero = ""
+        self.complemento = ""
+        self.cep = ""
+        self.creditCard = ""
+        self.cvv = ""
+        self.price = ""
+    }
+    
+    func clearCart () {
+        self.name = ""
+        self.url = ""
+        self.preenchimento = ""
+        self.espessura = ""
+        self.material = ""
+        self.quantidade = ""
+        self.rua = ""
+        self.numero = ""
+        self.complemento = ""
+        self.cep = ""
+        self.creditCard = ""
+        self.cvv = ""
+        self.price = ""
     }
     
     class func shared() -> Cart {
@@ -44,5 +82,93 @@ class Cart {
     
     func setUrl (url: String) -> Void {
         self.url = url
+    }
+    
+    func getPreenchimento () -> String{
+        return self.preenchimento
+    }
+    
+    func setUrl (preenchimento: String) -> Void {
+        self.preenchimento = preenchimento
+    }
+    
+    func getEspessura () -> String{
+        return self.espessura
+    }
+    
+    func setEspessura (espessura: String) -> Void {
+        self.espessura = espessura
+    }
+    
+    func getMaterial () -> String{
+        return self.material
+    }
+    
+    func setMaterial (material: String) -> Void {
+        self.material = material
+    }
+    
+    func getQuantidade () -> String{
+        return self.quantidade
+    }
+    
+    func setQuantidade (quantidade: String) -> Void {
+        self.quantidade = quantidade
+    }
+    
+    func getRua () -> String{
+        return self.rua
+    }
+    
+    func setRua (rua: String) -> Void {
+        self.rua = rua
+    }
+    
+    func getNumero () -> String{
+        return self.numero
+    }
+    
+    func setNumero (numero: String) -> Void {
+        self.numero = numero
+    }
+    
+    func getComplemento () -> String{
+        return self.complemento
+    }
+    
+    func setComplemento (complemento: String) -> Void {
+        self.complemento = complemento
+    }
+    
+    func getCep () -> String{
+        return self.cep
+    }
+    
+    func setCep (cep: String) -> Void {
+        self.cep = cep
+    }
+    
+    func getCreditCard () -> String{
+        return self.creditCard
+    }
+    
+    func setCreditCard (creditCard: String) -> Void {
+        self.creditCard = creditCard
+    }
+    
+    func getCvv () -> String{
+        return self.name
+    }
+    
+    func setCvv (cvv: String) -> Void {
+        self.cvv = cvv
+    }
+    
+    func getPrice () -> String{
+        return self.price
+    }
+    
+    func setPrice (price: String) -> Void {
+        self.price = price
     }
 }
