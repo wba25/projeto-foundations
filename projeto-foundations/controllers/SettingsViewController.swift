d6f09b78c4073f06c1dc53119812627ff1d23a3e
rdgdemelo <rdgdemelo@gmail.com>
2018-07-04 11:41:41 -0300

    created settings screen and controller

---
+++ projeto-foundations/controllers/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  projeto-foundations
+//
+//  Created by Rodrigo de Melo Silva on 04/07/18.
+//  Copyright © 2018 AcademyFoundations. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
