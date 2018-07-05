40ace06ae43d46748479fea0d99db2a6c64e3705
Hiago Rafael <hrns@cin.ufpe.br>
2018-07-05 10:09:24 -0300

    "Created file PaymentViewController"

---
+++ projeto-foundations/controllers/PaymentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PaymentViewController.swift
+//  projeto-foundations
+//
+//  Created by Hiago Rafael Nunes e Silva on 05/07/18.
+//  Copyright © 2018 AcademyFoundations. All rights reserved.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
